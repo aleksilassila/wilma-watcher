bef57dc582f218c329116e6f1f967f1e390d8a61
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2020-05-31 11:11:48 +0300

    Should not crash now

--- main.py
+++ main.py
@@ -13,34 +13,39 @@ class Wilma:
 		watch = {}
 
 	def login(self):
-		sessId = self.getSessionID()
+		def sendRequest():
+			sessId = self.getSessionID()
 
-		if sessId:
-			r = requests.post("https://wilma.espoo.fi/login", {
-				"Login": self.username,
-				"Password": self.password,
-				"SESSIONID": sessId
-			}, allow_redirects=False)
+			if sessId != None:
 
-			if ("Location" in r.headers and r.headers['Location'] == "https://wilma.espoo.fi/?loginfailed") or 499 >= r.status_code >= 400:
-				print("[-] Incorrect username/password")
+				try:
+					r = requests.post("https://wilma.espoo.fi/login", {
+						"Login": self.username,
+						"Password": self.password,
+						"SESSIONID": sessId
+					}, allow_redirects=False)
 
-				return False
+					if ("Location" in r.headers and r.headers['Location'] == "https://wilma.espoo.fi/?loginfailed") or 499 >= r.status_code >= 400:
+						print("[-] Incorrect username/password")
+
+						return False
+					else:
+						self.Wilma2SID = r.cookies.get_dict()["Wilma2SID"]
+						print(f"[+] Logged in as {self.username}, Wilma2SID: {self.Wilma2SID}")
+
+						return True
+				except Exception as e:
+					print(f"[–] Exception while logging in: {e}")
+					return False
 			else:
-				self.Wilma2SID = r.cookies.get_dict()["Wilma2SID"]
-				print(f"[+] Logged in as {self.username}, Wilma2SID: {self.Wilma2SID}")
+				return False
 
-				return True
-
-
-		else:
-			print("[-] Could not obtain session ID")
-			return False
-
+		while sendRequest() == False:
+			sleep(60*10)
 
 	def getSessionID(self):
-		r = requests.get("https://wilma.espoo.fi/login")
 		try:
+			r = requests.get("https://wilma.espoo.fi/login")
 			sessId = r.cookies.get_dict()["Wilma2LoginID"]
 			print(f"[+] Session ID: {sessId}")
 			return sessId
@@ -49,33 +54,37 @@ class Wilma:
 			return None
 
 	def checkCourse(self, courseId): # Return true if there is room in a course
-		r = requests.get(
-			f"https://wilma.espoo.fi/selection/getback?message=group-info&target={courseId}",
-			allow_redirects=False,
-			cookies={ "Wilma2SID": self.Wilma2SID }
-		)
+		try:
+			r = requests.get(
+				f"https://wilma.espoo.fi/selection/getback?message=group-info&target={courseId}",
+				allow_redirects=False,
+				cookies={ "Wilma2SID": self.Wilma2SID }
+			)
 
-		if r.status_code != 200:
-			print("[-] Error occured while checking course")
+			if r.status_code != 200:
+				print("[-] Error occured while checking course")
+				return None
+
+			soup = BeautifulSoup(r.text, features="html.parser")
+			tags = soup.find_all(["th", "td"])
+			name = soup.find_all("td", class_="coursename")[0].text
+
+			for index in range(0, len(tags)):
+				if tags[index].text == "Ilmoittautuneita":
+					ilmoittautuneita = tags[index + 1].text
+				elif tags[index].text == "Maksimikoko":
+					maksimikoko = tags[index + 1].text
+
+			if int(ilmoittautuneita) < int(maksimikoko):
+				print("\nKurssilla on tilaa!!\n")
+				self.sendPush(name, f"Kurssilla on tilaa: {ilmoittautuneita}/{maksimikoko}")
+				return True
+			else:
+				print(f"Ei tilaa: {ilmoittautuneita}/{maksimikoko}")
+				return False
+		except Exception as e:
+			print(f"Exception while checking course: {e}")
 			return None
-
-		soup = BeautifulSoup(r.text, features="html.parser")
-		tags = soup.find_all(["th", "td"])
-		name = soup.find_all("td", class_="coursename")[0].text
-
-		for index in range(0, len(tags)):
-			if tags[index].text == "Ilmoittautuneita":
-				ilmoittautuneita = tags[index + 1].text
-			elif tags[index].text == "Maksimikoko":
-				maksimikoko = tags[index + 1].text
-
-		if int(ilmoittautuneita) < int(maksimikoko):
-			print("\nKurssilla on tilaa!!\n")
-			self.sendPush(name, f"Kurssilla on tilaa: {ilmoittautuneita}/{maksimikoko}")
-			return True
-		else:
-			print(f"Ei tilaa: {ilmoittautuneita}/{maksimikoko}")
-			return False
 
 
 	def sendPush(self, name, message):
@@ -89,15 +98,18 @@ class Wilma:
 
 if __name__ == "__main__":
 	courses = os.environ.get('COURSEID').split(",")
-	w = Wilma()
-	w.login()
+	w = loginWilma()
+	w.()
 
 	while True:
 		for course in courses:
 			check = w.checkCourse(course)
-			if check == None:
+
+			while check == None:
 				w.login()
-				w.checkCourse(course)
+				check = w.checkCourse(course)
+				sleep(10*60)
+
 			sleep(1)
 
-		sleep(60*7)
+		sleep(60*8)
